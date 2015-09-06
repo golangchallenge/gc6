0e2d929081c020a630bcee0990ca622ed74018be
spf13 <steve.francia@gmail.com>
2015-09-06 00:28:26 -0400

    removing erroneous flag redefinition

--- commands/daedalus.go
+++ commands/daedalus.go
@@ -66,14 +66,6 @@ func init() {
 	rand.Seed(time.Now().UTC().UnixNano()) // need to initialize the seed
 	gin.SetMode(gin.ReleaseMode)
 
-	daedalusCmd.Flags().IntP("width", "w", 15, "width of the laybrinth")
-	daedalusCmd.Flags().IntP("height", "t", 10, "height of the laybrinth")
-
-	// Bind viper to these flags so viper can also read them from config, env, etc.
-	viper.SetDefault("width", 15)
-	viper.SetDefault("height", 10)
-	viper.BindPFlag("width", daedalusCmd.Flags().Lookup("width"))
-	viper.BindPFlag("height", daedalusCmd.Flags().Lookup("height"))
 	RootCmd.AddCommand(daedalusCmd)
 }
 
